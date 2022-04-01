4561d10bb06fffcd32db33694170d0d807915de4
prabessh <prabesh.dahal@innovatetech.co>
2022-04-01 12:03:08 +0545

    controller for total sale and top 3 countries

--- controllers/petroleum_stats_controller.js
+++ controllers/petroleum_stats_controller.js
@@ -1,5 +1,6 @@
 const PetroleumStat = require("../models/petroleum_stat");
 const fs = require("fs");
+const sequelize = require("../config/db");
 
 const getAll = async (req, res) => {
 	try {
@@ -28,4 +29,69 @@ const getAllFromFile = async (req, res) => {
 	});
 };
 
-module.exports = { getAll, getAllFromFile };
+const fetchTotalSale = async (req, res) => {
+	try {
+		// fetch rows of a group
+		const saleSum = await PetroleumStat.findAll({
+			attributes: [
+				"product",
+				[sequelize.fn("sum", sequelize.col("sale")), "total_sale"],
+			],
+			group: ["product"],
+			raw: true,
+		});
+		return res.status(200).send(saleSum);
+	} catch (error) {
+		return res.status(500).send(error.message);
+	}
+};
+
+const topCountry = async (req, res) => {
+	try {
+		// fetch rows of a group
+		const saleSum = await PetroleumStat.findAll({
+			attributes: [
+				"country",
+				[sequelize.fn("sum", sequelize.col("sale")), "total_sale"],
+			],
+			group: ["country"],
+			order: [[sequelize.col("total_sale"), "DESC"]],
+			limit: 3,
+			raw: true,
+		});
+		console.log(saleSum);
+
+		return res.status(200).send(saleSum);
+	} catch (error) {
+		return res.status(500).send(error.message);
+	}
+};
+
+const lowCountry = async (req, res) => {
+	try {
+		// fetch rows of a group
+		const saleSum = await PetroleumStat.findAll({
+			attributes: [
+				"country",
+				[sequelize.fn("sum", sequelize.col("sale")), "total_sale"],
+			],
+			group: ["country"],
+			order: [[sequelize.col("total_sale"), "ASC"]],
+			limit: 3,
+			raw: true,
+		});
+		console.log(saleSum);
+
+		return res.status(200).send(saleSum);
+	} catch (error) {
+		return res.status(500).send(error.message);
+	}
+};
+
+module.exports = {
+	getAll,
+	getAllFromFile,
+	fetchTotalSale,
+	topCountry,
+	lowCountry,
+};
--- routes/index.js
+++ routes/index.js
@@ -7,4 +7,10 @@ router.get("/index", petroleum_stats_controller.getAll);
 
 router.get("/file_data", petroleum_stats_controller.getAllFromFile);
 
+router.get("/total_sale", petroleum_stats_controller.fetchTotalSale);
+
+router.get("/top_country", petroleum_stats_controller.topCountry);
+
+router.get("/low_country", petroleum_stats_controller.lowCountry);
+
 module.exports = router;
